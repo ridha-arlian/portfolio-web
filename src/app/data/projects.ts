eb2fd1c1e5a6726c2308bd66bccf0f5f80fe4d75
Ridha Arlian Alhaqie <ridhaarlian@proton.me>
2025-09-02 15:59:58 +0700

    refactor(project): update data url

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,7 +3,7 @@ export const projectsData = [
     title: 'One Day, One Sentence',
     description: 'A web diary inspired by the Homework for Life method, built with Next.js, Chakra UI, and Google Spreadsheet integration.',
     tags: ['Next.js', 'Spreadsheet', 'Google', 'Chakra-UI'],
-    url: 'https://catardot.vercel.app',
+    url: 'https://catardot.vercel.app/',
   },
   // {
   //   title: 'Cross-Chain Bridge',
