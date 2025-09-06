f2de23817a3e8dd3cd361156abb86c02f84d23cb
ridha-arlian <ridhaarlian@proton.me>
2025-09-06 15:34:24 +0700

    refactor: remove unused code

--- src/components/icons/iconsLightMode.tsx
+++ src/components/icons/iconsLightMode.tsx
@@ -12,7 +12,7 @@ export const Mediasouplighticons = chakra((props: React.ComponentProps<"svg">) =
 
 export const XLighticons = chakra((props: React.ComponentProps<"svg">) => (
   <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="250px" height="250px" fillRule="nonzero" {...props}>
-    <g fill="#000000" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: "normal" }}>
+    <g fill="#000000" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" style={{ mixBlendMode: "normal" }}>
       <g transform="scale(5.12,5.12)">
         <path d="M5.91992,6l14.66211,21.375l-14.35156,16.625h3.17969l12.57617,-14.57812l10,14.57813h12.01367l-15.31836,-22.33008l13.51758,-15.66992h-3.16992l-11.75391,13.61719l-9.3418,-13.61719zM9.7168,8h7.16406l23.32227,34h-7.16406z"></path>
       </g>
