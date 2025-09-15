715c5294fc24ffcdaadc411b7eed7f5dad995d71
ridha-arlian <ridhaarlian@proton.me>
2025-09-15 08:02:58 +0700

    refactor(project): update data in project.ts

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -5,4 +5,10 @@ export const projectsData = [
     tags: ["Next.js", "Spreadsheet", "Google", "Chakra-UI"],
     url: "https://catardot.vercel.app/",
   },
+  {
+    title: "Thesis: Online Exam Proctoring Feature with WebRTC",
+    description: "Developed a proctoring feature for CBT using WebRTC and mediasoup, with three integrated applications for participants, proctors, and admins.",
+    tags: ["WebRTC", "Mediasoup", "React.js", "Node.js", "Django"],
+    url: "#",
+  }
 ]
