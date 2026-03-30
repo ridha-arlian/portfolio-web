0b4fbdde62a221a5e080d96012f10f8f7e7e0a93
ridha-arlian <ridhaarlian@proton.me>
2026-03-30 21:13:01 +0700

    feat(project): update project link

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,7 +3,7 @@ export const projectsData = [
     title: "Catardot",
     description: "A web diary inspired by the Homework for Life method, built with Next.js, Chakra UI, and Google Spreadsheet integration.",
     tags: ["Next.js", "Spreadsheet", "Google", "Chakra-UI"],
-    url: "https://catardot.vercel.app/",
+    url: "https://catardot.ridhaarlian.my.id/",
   },
   {
     title: "Thesis: Online Exam Proctoring Feature with WebRTC",
@@ -15,6 +15,6 @@ export const projectsData = [
     title: "Medic.in",
     description: "A System Information for Clinic Management built with Laravel and Filament 4.",
     tags: ["Laravel", "Filament 4", "PHP", "PostgreSQL"],
-    url: "https://medicin.up.railway.app/",
+    url: "https://medicin.ridhaarlian.my.id/",
   }
 ]
